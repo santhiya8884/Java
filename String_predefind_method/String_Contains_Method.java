5326f0d768b8da5f985c37cb299fd4c5708e469d
santhiya8884 <sanammu91@gmail.com>
2021-11-06 23:44:35 +0530

    contains Method

---
+++ String_predefind_method/String_Contains_Method.java
@@ -0,0 +1,20 @@
+package string_practice;
+
+public class String_Contains_Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Santhiya";
+		System.out.println(s.contains("San"));
+		System.out.println(s.contains("zara"));
+		System.out.println(s.contains("r"));
+		System.out.println(s.contains("e"));
+	}
+
+}
+/* output :
+true
+false
+false
+false
+*/
