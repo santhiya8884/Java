bcbed743b2f5381896ac881ed7b2cb0244b87120
santhiya8884 <sanammu91@gmail.com>
2021-11-07 00:24:18 +0530

    indexOf() using char  Method

---
+++ String_predefind_method/String_indexOf_Char_Method.java
@@ -0,0 +1,20 @@
+package string_practice;
+
+public class String_indexOf_Char_Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="santhiya ";
+		System.out.println(s.indexOf(0));
+		System.out.println(s.indexOf('n'));
+//		System.out.println(s.indexOf(''));
+		System.out.println(s.indexOf(' '));
+	}
+
+}
+/* output :
+-1
+2
+8
+
+*/
