6965dbac2c0dba94b560e60ca8228a5cf44819b5
santhiya8884 <sanammu91@gmail.com>
2021-11-06 23:54:34 +0530

    length() Method

---
+++ String_predefind_method/String_Length_Method.java
@@ -0,0 +1,22 @@
+package string_practice;
+
+public class String_Length_Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="santhiya";
+		String s1=" ";
+		String s2="";
+		System.out.println(s+" Length of the string is "+s.length());
+		System.out.println(s1+" Length of the string is "+s1.length());
+		System.out.println(s2+" Length of the string is "+s2.length());
+	}
+
+}
+/* output :
+ 
+santhiya Length of the string is 8
+  Length of the string is 1
+ Length of the string is 0
+
+*/
