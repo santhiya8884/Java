b0f7e9373229039ed50d0bc055127fc77b56315a
santhiya8884 <sanammu91@gmail.com>
2021-11-06 23:57:32 +0530

    toLowerCase()  Method

---
+++ String_predefind_method/String_To_LowerCase_MEthod.java
@@ -0,0 +1,17 @@
+package string_practice;
+
+public class String_To_LowerCase_MEthod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="SaNthiya";
+		String s1=" ";
+		System.out.println("Lower case string "+s.toLowerCase());
+		System.out.println("Lower case string "+s1.toLowerCase());
+	}
+
+}
+/* output :
+Lower case string santhiya
+Lower case string  
+*/
