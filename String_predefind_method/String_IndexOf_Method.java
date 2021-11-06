2a3006c784a1e02a982a9828f4ca4667aa587149
santhiya8884 <sanammu91@gmail.com>
2021-11-07 00:08:43 +0530

    indexOf()  Method

---
+++ String_predefind_method/String_IndexOf_Method.java
@@ -0,0 +1,22 @@
+package string_practice;
+
+public class String_IndexOf_Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Santhiya";
+		System.out.println(s.indexOf(0));
+		System.out.println(s.indexOf("S"));
+		System.out.println(s.indexOf("s"));
+		System.out.println(s.indexOf("San"));
+		System.out.println(s.indexOf("z"));
+	}
+
+}
+/* output :
+-1
+0
+-1
+0
+-1
+*/
