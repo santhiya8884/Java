3c88db08b3d51f3bc7197b7f064d796fe71bb4ec
santhiya8884 <sanammu91@gmail.com>
2021-11-06 23:35:58 +0530

    concatMethod

---
+++ String_predefind_method/String_Predefind_Methods.java
@@ -0,0 +1,22 @@
+package string_practice;
+
+public class String_Predefind_Methods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String_Predefind_Methods	sara=new String_Predefind_Methods();
+		sara.concatMethod();
+	}
+
+	public void concatMethod() {
+		// TODO Auto-generated method stub
+		String s="santhiya";
+		s=s.concat(" is good girl");
+		System.out.println(s);
+	}
+
+}
+/* output :
+ santhiya is good girl
+ 
+ */
