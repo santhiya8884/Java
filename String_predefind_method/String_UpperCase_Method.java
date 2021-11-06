564f7cdfc6187e11ed5e864df5d35489b9493ebd
santhiya8884 <sanammu91@gmail.com>
2021-11-06 23:59:52 +0530

    toUpperCase()  Method

---
+++ String_predefind_method/String_UpperCase_Method.java
@@ -0,0 +1,14 @@
+package string_practice;
+
+public class String_UpperCase_Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="santhiYA";
+		System.out.println(" Upper case String "+s.toUpperCase());
+	}
+
+}
+/* output :
+Upper case String SANTHIYA
+*/
