e82b3bfc33fec85ce4df7175952a31c0a0864b82
santhiya8884 <sanammu91@gmail.com>
2021-11-07 00:29:24 +0530

    lastIndexOf() using char  Method

---
+++ String_predefind_method/String_lstIndexOf_Char_Method.java
@@ -0,0 +1,20 @@
+package string_practice;
+
+public class String_lstIndexOf_Char_Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="santhiya";
+		System.out.println(s.lastIndexOf('a'));
+		//System.out.println(s.lastIndexOf('  '));
+	//	System.out.println(s.lastIndexOf(''));
+		System.out.println(s.lastIndexOf('z'));
+		System.out.println(s.lastIndexOf('*'));
+	}
+
+}
+/* output :
+7
+-1
+-1
+*/
