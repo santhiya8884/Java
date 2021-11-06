a1d0213d3dd2a3674da0a3234a39f1ab6a6aecba
santhiya8884 <sanammu91@gmail.com>
2021-11-06 23:47:42 +0530

    startsWith Method

---
+++ String_predefind_method/String_Start_With_Method.java
@@ -0,0 +1,20 @@
+package string_practice;
+
+public class String_Start_With_Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Santhiya";
+		System.out.println(s.startsWith("A"));
+		System.out.println(s.startsWith("San"));
+		System.out.println(s.startsWith("S"));
+		System.out.println(s.startsWith("s"));
+	}
+
+}
+/* output :
+false
+true
+true
+false
+*/
