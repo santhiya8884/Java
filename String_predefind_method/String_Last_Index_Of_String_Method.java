9580badb682dabd7824fbb6b1cd1501fc12be7a6
santhiya8884 <sanammu91@gmail.com>
2021-11-07 00:13:02 +0530

    lastIndexOf() String  Method

---
+++ String_predefind_method/String_Last_Index_Of_String_Method.java
@@ -0,0 +1,25 @@
+package string_practice;
+
+public class String_Last_Index_Of_String_Method {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="Santhiya";
+		System.out.println(s.lastIndexOf(0));
+		System.out.println(s.lastIndexOf("S"));
+		System.out.println(s.lastIndexOf("s"));
+		System.out.println(s.lastIndexOf("San"));
+		System.out.println(s.lastIndexOf("z"));
+		System.out.println(s.lastIndexOf("a"));
+		System.out.println(s.lastIndexOf("iya"));
+	}
+
+}
+/* output :-1
+0
+-1
+0
+-1
+7
+5
+*/
