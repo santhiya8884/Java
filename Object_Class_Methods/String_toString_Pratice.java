ddb4caa801864e66b2c46acf2e5188d93102fdfc
santhiya8884 <sanammu91@gmail.com>
2021-11-13 20:13:59 +0530

    hashCode()

---
+++ Object_Class_Methods/String_toString_Pratice.java
@@ -0,0 +1,23 @@
+package string_13_11_2021;
+
+public class String_toString_Pratice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String_toString_Pratice sara=new String_toString_Pratice();
+		System.out.println(sara.hashCode());
+		System.out.println(sara.toString());
+		System.out.println(sara);
+		String ss=new String("santhiya");
+		String s="vani";
+		System.out.println(s);
+		
+	}
+
+}
+/* output :
+1950409828
+string_13_11_2021.String_toString_Pratice@7440e464
+string_13_11_2021.String_toString_Pratice@7440e464
+vani
+*/
