baed28df457a199e6621684532ad2fdd00ef4a1a
santhiya8884 <sanammu91@gmail.com>
2021-11-14 23:51:50 +0530

    HashCode practice and equals method pratice

---
+++ Object_Class_Methods/SetPractice.java
@@ -0,0 +1,24 @@
+package coll_14_11_2021;
+
+import java.util.ArrayList;
+
+public class SetPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SetPractice s1=new SetPractice();
+		SetPractice s2=new SetPractice();
+		ArrayList  al=new ArrayList();
+		al.add(s1);
+		al.add(s2);
+		System.out.println(al);
+		
+	}
+	public String toString() { 
+		return "HAi";
+	}
+
+}
+/* output :
+[HAi, HAi]
+*/
