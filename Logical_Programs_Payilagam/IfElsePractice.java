5511eef7a3965fb4db429383b0ca84d06bf5cc60
santhiya8884 <sanammu91@gmail.com>
2021-11-19 12:00:27 +0530

    if else  condition

---
+++ Logical_Programs_Payilagam/IfElsePractice.java
@@ -0,0 +1,20 @@
+package payailagam_logical_programs;
+
+public class IfElsePractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("******if  else pratcie***** ");
+		int value=5;
+		if(value==5) { 
+			System.out.println("Hai");
+		}
+		else {
+			System.out.println("BYe");
+	}
+
+}
+}/* output :
+******if  else pratcie***** 
+Hai
+*/
