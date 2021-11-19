071661a59886ce7e48d423fd1b9302d2fea14255
santhiya8884 <sanammu91@gmail.com>
2021-11-19 11:55:19 +0530

    If cindition

---
+++ Logical_Programs_Payilagam/If_Pratice.java
@@ -0,0 +1,21 @@
+package payailagam_logical_programs;
+
+public class If_Pratice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int value=5;
+		if(true) { 
+			System.out.println("Hai");
+		}
+		if(value==5) { 
+			System.out.println("BYe");
+		}
+
+	}
+
+}
+/* output" :
+Hai
+BYe
+*/
