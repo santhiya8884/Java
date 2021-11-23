0989ec7ad90954d936a4d61209e3030c678ab21b
santhiya8884 <sanammu91@gmail.com>
2021-11-23 13:52:53 +0530

    Throws KEyword used

---
+++ Exception_Handling_Examples/Thows_keyword/ThrowsDemo.java
@@ -0,0 +1,17 @@
+package utility_classes;
+
+public class ThrowsDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+	public static void divide(int a,  int b)throws ArithmeticException , ArrayIndexOutOfBoundsException{ 
+		//int x=a,y=b;
+		int c=a/b;
+		System.out.println(c);
+		
+	}
+
+}
