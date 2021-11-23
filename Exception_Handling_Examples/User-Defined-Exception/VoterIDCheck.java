111c64144441a7097ea129bebb7d31526ae7914f
santhiya8884 <sanammu91@gmail.com>
2021-11-23 15:20:39 +0530

    Check

---
+++ Exception_Handling_Examples/User-Defined-Exception/VoterIDCheck.java
@@ -0,0 +1,15 @@
+package excption_practice_23_11_2021;
+
+public class VoterIDCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UserDefinedException.verifyAge(15);
+	}
+
+}
+/* output :
+Age 15
+Please check your age:15
+
+*/
