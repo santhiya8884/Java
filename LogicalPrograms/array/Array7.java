64fceab74d7843263b99e371666a54bbd92b6078
santhiya8884 <sanammu91@gmail.com>
2021-12-16 19:33:09 +0530

    Linear search

--- LogicalPrograms/array/Array7.java
+++ LogicalPrograms/array/Array7.java
@@ -5,20 +5,27 @@ public class Array7{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int array[]= {10,20,30,40,50};
-		int key=50;
+		int key=30;
 		int i;
 		for(i=0;i<array.length;i++) { 
 			if(key==array[i]) {
 				break;	
 			}
 		}
+		if(i==array.length) {
+			System.out.println("Element not present in the array");
+		}
+		else {
+			System.out.println("Element present in the location "+i);
+		}
 		
-		System.out.println("Element present in the location "+i);
+		
+		
 		
 		
 	}
 
 }
 /* output :
-Element present in the location 4
+Element present in the location 2
 */
