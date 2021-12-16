7f55ca838838315cbabb680ef4e8a6ff881fa44f
santhiya8884 <sanammu91@gmail.com>
2021-12-16 17:42:56 +0530

    Priting a Array

---
+++ LogicalPrograms/array/Array2.java
@@ -0,0 +1,19 @@
+package arrayPracticePayialagam;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[]= {10,20,30,40};
+		for(int i=0;i<array.length;i++) { 
+			System.out.println(array[i]);
+		}
+	}
+
+}
+/* output :
+10
+20
+30
+40
+*/
