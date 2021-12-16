502aec1c3a8d517a81f110801c3c252d9ca559c6
santhiya8884 <sanammu91@gmail.com>
2021-12-16 17:45:09 +0530

    Reverse priting a  Array

---
+++ LogicalPrograms/array/Array3.java
@@ -0,0 +1,20 @@
+package arrayPracticePayialagam;
+
+public class Array3{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[]= {10,20,30,40};
+		for(int i=array.length-1;i>=0;i--) { 
+			System.out.println(array[i]);
+		}
+	}
+
+}
+/* output :
+40
+30
+20
+10
+
+*/
