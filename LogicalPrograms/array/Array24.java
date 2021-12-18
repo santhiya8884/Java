8b9a2b28d033bfafa9607ad7239ad5dd7e8e7b45
santhiya8884 <sanammu91@gmail.com>
2021-12-18 19:00:22 +0530

    Each element how many time present in the array

---
+++ LogicalPrograms/array/Array24.java
@@ -0,0 +1,47 @@
+package arrayPracticePayialagam;
+
+import java.util.Scanner;
+
+public class Array24{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Array24 sara=new Array24();
+		System.out.println("Each element how many times present in the array :");
+		sara.method1();
+		
+	}
+	private void method1() {
+		// TODO Auto-generated method stub
+		int array[]= {10,20,10,20,50};//0,1,2,3,4,.............
+		int freq[]=new int[array.length];
+		
+		for(int i=0;i<array.length;i++) { 
+			int no=array[i];
+			int count=1;
+			for(int j=i+1;j<array.length;j++) { 
+				if(no==array[j]) {
+					freq[j]=-5;
+					count++;
+				}
+			}
+			if(freq[i]!=-5) { 
+				freq[i]=count;
+			}
+		}
+		for(int i=0;i<freq.length;i++) { 
+			if(freq[i]>0) { 
+				System.out.println(array[i]+" "+freq[i]+" times ");
+			}
+		}
+		
+		
+}
+	
+}
+/* output :
+Each element how many times present in the array :
+10 2 times 
+20 2 times 
+50 1 times 
+*/
