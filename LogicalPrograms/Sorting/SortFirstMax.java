56b1e26b3ec86349880c91c1cfa63b96169e0dae
santhiya8884 <sanammu91@gmail.com>
2021-12-19 16:31:07 +0530

    Sort using find MAX

---
+++ LogicalPrograms/Sorting/SortFirstMax.java
@@ -0,0 +1,40 @@
+package searching;
+
+public class SortFirstMax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[]= {90,30,50,10,70,30};
+		//System.out.println("Bubbe Sort");
+		System.out.println("Sort using find MAx");
+		for(int i=0;i<array.length;i++) { 
+			//System.out.print(array[i]+" ");
+		}
+		System.out.println();
+		int temp;
+		int j=1;
+		while(j<array.length-1) {
+			int i=0;
+		for(;i<array.length-j;i++) { 
+			if(array[i]>array[i+1]) { 
+				temp=array[i];
+				array[i]=array[i+1];
+				array[i+1]=temp;
+			}
+		}
+		j++;
+		}
+		//System.out.println("After Sorting");
+		for(int i=0;i<array.length;i++) { 
+			//System.out.print(array[i]+" ");
+		}
+		System.out.println(array[array.length-1]);
+	}
+
+}
+/* output
+Sort using find MAx
+
+90
+
+*/
