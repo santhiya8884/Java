fad9daca243da34ceb71488b9eac6705814c739e
santhiya8884 <sanammu91@gmail.com>
2021-12-20 17:00:24 +0530

    Convert to  uppercase from given string

---
+++ LogicalPrograms/String/String14.java
@@ -0,0 +1,23 @@
+package string;
+
+import java.util.Scanner;
+
+public class String14 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="sara santhiya. Wecome";
+		System.out.println("convert to uppercase from given string");
+		System.out.println(s.toUpperCase());
+		
+	}
+
+	
+}
+/* output :
+convert to uppercase from given string
+SARA SANTHIYA. WECOME
+
+
+
+*/
