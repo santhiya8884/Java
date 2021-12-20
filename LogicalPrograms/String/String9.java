f80416fab3cdb2f59b34c87a2e96a8b6af8e15da
santhiya8884 <sanammu91@gmail.com>
2021-12-20 16:09:30 +0530

    String to int

---
+++ LogicalPrograms/String/String9.java
@@ -0,0 +1,23 @@
+package string;
+
+import java.util.Scanner;
+
+public class String9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("String to int..");
+		String s="20";
+		//String s1="sara";    //Runtime error ----- number format exception
+		int a=Integer.parseInt(s);
+		System.out.println(a);
+		
+		
+	}
+
+}
+/* output :
+String to int..
+20
+
+*/
