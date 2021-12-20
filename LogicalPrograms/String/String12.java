809319c49dcb50c836094ac5eb4fa99f75c85678
santhiya8884 <sanammu91@gmail.com>
2021-12-20 16:53:44 +0530

    Count of words

---
+++ LogicalPrograms/String/String12.java
@@ -0,0 +1,23 @@
+package string;
+
+import java.util.Scanner;
+
+public class String12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="sara santhiya. Welcome";
+		System.out.println("Countof words");
+		String s1[]=s.split("\\s");
+		System.out.println(s1.length);
+		
+	}
+
+	
+}
+/* output :
+Countof words
+3
+
+
+*/
