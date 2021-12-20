228536783294a0d8ad384098d359433f18627d33
santhiya8884 <sanammu91@gmail.com>
2021-12-20 16:34:04 +0530

    charArray to String

---
+++ LogicalPrograms/String/String11.java
@@ -0,0 +1,26 @@
+package string;
+
+import java.util.Scanner;
+
+public class String11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch[]= {'s','a','r','a'};
+		System.out.println("charArray to String");
+		System.out.println(ch);
+		//String s=ch.toString();
+		
+		String s=String.valueOf(ch);
+		System.out.println(s);
+		
+	}
+
+	
+}
+/* output :
+charArray to String
+sara
+sara
+
+*/
