8332e89ffa26981d73fe34fe085303ca0e6e03e9
santhiya8884 <sanammu91@gmail.com>
2021-12-20 09:30:03 +0530

    Find the duplicate charcters present in the string

---
+++ LogicalPrograms/String/String5.java
@@ -0,0 +1,60 @@
+package string;
+
+import java.util.Scanner;
+
+public class String5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="sara welcome  12345321";
+		
+		System.out.println("Duplicate charcters present in the string");
+		char[] ch=new char[s.length()];
+		for(int i=0;i<s.length();i++) {
+			 ch[i]=s.charAt(i);
+		}
+		for(int i=0;i<ch.length;i++) { 
+			System.out.print(ch[i]+"  ");
+		}
+		System.out.println();
+		int count=0;
+		int freq[]=new int[ch.length];
+		for(int i=0;i<ch.length;i++) { 
+			count=1;
+			for(int j=i+1;j<ch.length;j++) { 
+				if(ch[i]==ch[j]) { 
+					count++;
+					freq[j]=-1;
+				}
+			}
+			if(freq[i]!=-1) { 
+				freq[i]=count;
+			}
+		}
+		for(int i=0;i<freq.length;i++) { 
+			System.out.print(freq[i]+" |");
+		}
+		System.out.println();
+		for(int i=0;i<ch.length;i++) { 
+			if(freq[i]>1) { 
+				System.out.println(ch[i]+" present at "+freq[i]+" times");
+			}
+		}
+		
+		
+	}
+
+}
+/* output :
+Duplicate charcters present in the string
+s  a  r  a     w  e  l  c  o  m  e        1  2  3  4  5  3  2  1  
+1 |2 |1 |-1 |3 |1 |2 |1 |1 |1 |1 |-1 |-1 |-1 |2 |2 |2 |1 |1 |-1 |-1 |-1 |
+a present at 2 times
+  present at 3 times
+e present at 2 times
+1 present at 2 times
+2 present at 2 times
+3 present at 2 times
+
+
+*/
