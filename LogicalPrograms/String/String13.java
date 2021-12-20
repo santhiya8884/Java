5844d91ea1b7cd5738316c02bd3af320ba5e243e
santhiya8884 <sanammu91@gmail.com>
2021-12-20 16:58:21 +0530

    Convert to first charcter to uppercase

---
+++ LogicalPrograms/String/String13.java
@@ -0,0 +1,22 @@
+package string;
+
+import java.util.Scanner;
+
+public class String13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="sara santhiya. Wecome";
+		System.out.println("covert to first charcter only uppercase");
+		System.out.println(s.substring(0,1).toUpperCase()+s.substring(1));
+		
+	}
+
+	
+}
+/* output :
+covert to first charcter only uppercase
+Sara santhiya. Wecome
+
+
+*/
